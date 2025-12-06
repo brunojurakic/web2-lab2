237be881fd67937fff26bf14b242db8fb440dd8a
Bruno Jurakić <178019478+brunojurakic@users.noreply.github.com>
2025-12-06 23:18:30 +0100

    fix drizzle export

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
+const config = {
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-});
+};
+
+export default config;
